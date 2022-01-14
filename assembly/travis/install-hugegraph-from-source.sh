446f74f6e90ab3c343e87fc522cacbd59ee38c1b
wenchuanbo <wenchuanbo@baidu.com>
2022-01-14 18:06:31 +0800

    update ci for 3.0.0

--- assembly/travis/install-hugegraph-from-source.sh
+++ assembly/travis/install-hugegraph-from-source.sh
@@ -12,7 +12,17 @@ HUGEGRAPH_GIT_URL="https://github.com/starhugegraph/hugegraph.git"
 
 git clone ${HUGEGRAPH_GIT_URL}
 cd hugegraph
-git checkout -b gh-pre-release origin/gh-pre-release
+git checkout -b gh-dis-release origin/gh-dis-release
+
+# install lib
+TRAVIS_DIR="hugegraph-dist/src/assembly/travis/lib"
+mvn install:install-file -Dfile=$TRAVIS_DIR/hg-pd-client-3.0.0.jar -DgroupId=com.baidu.hugegraph -DartifactId=hg-pd-client -Dversion=3.0.0 -Dpackaging=jar  -DpomFile=$TRAVIS_DIR/hg-pd-client-pom.xml
+mvn install:install-file -Dfile=$TRAVIS_DIR/hg-pd-common-3.0.0.jar -DgroupId=com.baidu.hugegraph -DartifactId=hg-pd-common -Dversion=3.0.0 -Dpackaging=jar -DpomFile=$TRAVIS_DIR/hg-pd-common-pom.xml
+mvn install:install-file -Dfile=$TRAVIS_DIR/hg-pd-grpc-3.0.0.jar -DgroupId=com.baidu.hugegraph -DartifactId=hg-pd-grpc -Dversion=3.0.0 -Dpackaging=jar -DpomFile=$TRAVIS_DIR/hg-pd-grpc-pom.xml
+mvn install:install-file -Dfile=$TRAVIS_DIR/hg-store-client-3.0.0.jar -DgroupId=com.baidu.hugegraph -DartifactId=hg-store-client -Dversion=3.0.0 -Dpackaging=jar -DpomFile=$TRAVIS_DIR/hg-store-client-pom.xml
+mvn install:install-file -Dfile=$TRAVIS_DIR/hg-store-grpc-3.0.0.jar -DgroupId=com.baidu.hugegraph -DartifactId=hg-store-grpc -Dversion=3.0.0 -Dpackaging=jar -DpomFile=$TRAVIS_DIR/hg-store-grpc-pom.xml
+mvn install:install-file -Dfile=$TRAVIS_DIR/hg-store-term-3.0.0.jar -DgroupId=com.baidu.hugegraph -DartifactId=hg-store-term -Dversion=3.0.0 -Dpackaging=jar -DpomFile=$TRAVIS_DIR/hg-store-term-pom.xml
+
 mvn package -DskipTests
 mv hugegraph-*.tar.gz ../
 cd ../
